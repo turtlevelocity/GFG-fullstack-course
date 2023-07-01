f13ee85990086f0191269fe31e8e0cdf47cee372
Shubham Mishra <shubh.ism310@gmail.com>
2023-07-02 01:19:45 +0530

    learn basics javascript

---
+++ index.js
@@ -0,0 +1,113 @@
+// var name = "Shubham"
+
+// var number = 100
+// console.log(number)
+
+// let, const
+// let name = "Shubham"
+// console.log(name)
+
+// const number = 1000
+// console.log(number)
+
+// var , let, const
+// let variable = 5;
+// const varible2 = 10
+
+// console.log(variable1);
+// console.log(variable1)
+
+// console.log(varible2)
+
+// it cannot start with a number : not valid 1variable 
+// it cannot contain space or -
+// few reserved words you cannot use : if, else, while, etc
+
+// -------
+// Comparisons, Conditions
+// >, < , >=, <=
+
+// if( 10 > 20) {
+//   console.log("10 > 20")
+// }
+
+// else {
+//   console.log("10 < 20")
+// }
+
+// Loops
+// for(let i=1; i<10;i++) {
+//   console.log(i)
+//   // i++ = i = i+1 = 2+1 = 3, i= 10
+
+// }
+// console.log("end for loop")
+
+// let i=1;
+// while(i<10){
+//   console.log(i)
+//   i++
+// }
+// console.log("end while loop")
+
+// string
+let name = "1"
+// number
+let number = null
+// boolean
+// let boolVariable = false
+// console.log(number)
+// if(boolVariable){
+//   console.log("inside if condition")
+// }
+
+// Reference Types
+// 1. object
+// let personName = "Shubham"
+// let age = 28
+// let person1 = {
+//   name: "Shubham",
+//   age: 27
+// }
+
+// let person2 = {
+//   name: "Person2",
+//   age: 30
+// }
+
+// console.log(person1.name)
+// console.log(person1["age"])
+
+// console.log(person2.name)
+// console.log(person2["age"])
+
+
+// 2. Arrays
+// let arrayVariable = ["1", 2, 3.0,4,"5", "shubham"]
+// console.log(arrayVariable[5])
+
+// 3. function
+
+// add any 2 numbers
+
+// function addNumber(num1, num2) {
+//   let sum = num1+num2;
+//   console.log(sum)
+// }
+
+// addNumber(1,2)
+
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.question("Please enter a value: ", (value) => {
+  console.log(
+    `total count of prime numbers  till ${value}
+    )}`
+  );
+  reader.close();
+});
